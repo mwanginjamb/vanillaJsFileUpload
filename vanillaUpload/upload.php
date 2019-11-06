d72595f76d23633355f9d418faa550aed1d5de6a
mwanginjamb <francnjamb@gmail.com>
2019-11-06 11:26:27 +0300

    Added some comments to the code . just for context purposes

--- vanillaUpload/upload.php
+++ vanillaUpload/upload.php
@@ -1,10 +1,17 @@
 <?php
+/*
+    Written by francnjamb@gmail.com
+    A demo just showing how post data from the js frontend looks like.
+*/
 print '<pre>';
+
+
+#Get the post data -- none multipart
 print_r($_POST);
 
 
 print '<hr>';
-
+#Get the files array
 print_r($_FILES);
 
 ?>
